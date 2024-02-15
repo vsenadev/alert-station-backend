68b2ecaa33fe9b8eb57a4358eebb252af1f2ff7c
Vinicius Sena <senavinicius01@gmail.com>
2024-02-15 20:48:46 -0300

    FIX - Properties for class correction

--- src/repositories/TrainsSituation.repository.ts
+++ src/repositories/TrainsSituation.repository.ts
@@ -12,45 +12,43 @@ import { format, parseISO } from 'date-fns';
 
 @Injectable()
 export class TrainsSituationRepository {
+  private readonly lines = {
+    1: 'Azul',
+    2: 'Verde',
+    3: 'Vermelha',
+    4: 'Amarela',
+    5: 'Lilás',
+    7: 'Rubi',
+    8: 'Diamante',
+    9: 'Esmeralda',
+    10: 'Turquesa',
+    11: 'Coral',
+    12: 'Safira',
+    13: 'Jade',
+    15: 'Prata',
+    17: 'Ouro',
+  };
+  private readonly colors = {
+    1: '#00378C',
+    2: '#186D55',
+    3: '#F51200',
+    4: '#EFBA00',
+    5: '#9271B1',
+    7: '#C80857',
+    8: '#949488',
+    9: '#219896',
+    10: '#1B85A5',
+    11: '#F46C55',
+    12: '#1F2086',
+    13: '#29B352',
+    15: '#848D90',
+    17: 'rgb(255, 215, 0)',
+  };
+
   constructor(
     @InjectModel(TrainsSituationModelName)
     private readonly trainsSituationModel: Model<TrainsSituation>,
-    private readonly lines: ILines,
-    private readonly colors: ILines,
-  ) {
-    this.lines = {
-      1: 'Azul',
-      2: 'Verde',
-      3: 'Vermelha',
-      4: 'Amarela',
-      5: 'Lilás',
-      7: 'Rubi',
-      8: 'Diamante',
-      9: 'Esmeralda',
-      10: 'Turquesa',
-      11: 'Coral',
-      12: 'Safira',
-      13: 'Jade',
-      15: 'Prata',
-      17: 'Ouro',
-    };
-    this.colors = {
-      1: '#00378C',
-      2: '#186D55',
-      3: '#F51200',
-      4: '#EFBA00',
-      5: '#9271B1',
-      7: '#C80857',
-      8: '#949488',
-      9: '#219896',
-      10: '#1B85A5',
-      11: '#F46C55',
-      12: '#1F2086',
-      13: '#29B352',
-      15: '#848D90',
-      17: 'rgb(255, 215, 0)',
-    };
-  }
+  ) {}
 
   async postTrainsStatus(trainsSituation: ITrainsSituation[]): Promise<void> {
     for (const element of trainsSituation) {
